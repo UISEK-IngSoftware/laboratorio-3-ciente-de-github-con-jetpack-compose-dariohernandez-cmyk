8014e44fcab0e7b1e0476c991f246ba5d5392416
Dario <dario.hernandez@uisek.edu.ec>
2026-05-21 00:08:12 -0500

    Finalizacion Lab # 4 D Hernandez

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,15 @@ android {
         compose = true
         buildConfig = true
     }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/INDEX.LIST"
+            excludes += "META-INF/io.netty.versions.properties"
+            excludes += "META-INF/DEPENDENCIES"
+        }
+    }
 }
 
 dependencies {
